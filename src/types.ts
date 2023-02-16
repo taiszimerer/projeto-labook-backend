851c9aa6a30d71f49ebd5d26ba894c0293e4894a
taiszimerer <taiszimerer@gmail.com>
2023-02-16 10:59:52 -0300

    feat:criação da pasta models com as tipagens e alteração na pasta types

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,28 @@
-export type TUser = {
-    id: string,
-    name: string,
-    email: string,
-    password: string,
-    role: string,
-    created_at: string
+export enum USER_ROLES {
+    NORMAL = "NORMAL",
+    ADMIN = "ADMIN"
 }
 
-export type TPosts = {
+export interface TokenPayload {
+    id: string,
+    name: string,
+    role: USER_ROLES
+}
+
+export interface PostModel {
+    id: string,
+    content: string,
+    likes: number,
+    dislikes: number,
+    createdAt: string,
+    updatedAt: string,
+    creator: {
+        id: string,
+        name: string
+    }
+}
+
+export interface PostDB {
     id: string,
     creator_id: string,
     content: string,
@@ -17,7 +32,28 @@ export type TPosts = {
     updated_at: string
 }
 
-export type TLikesDeslikes = {
+
+export interface UserDB {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: string,
+    created_at: string
+}
+
+export interface UserModel {
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    role: string,
+    createdAt: string
+}
+
+
+
+export interface LikesDeslikesDB {
     user_id: string,
     post_id: string,
     like: number
